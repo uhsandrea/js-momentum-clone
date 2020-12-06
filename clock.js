edb9dd6d36cccdb00742a2c0041ef9d2ddc7dc82
uhsandrea <74327083+uhsandrea@users.noreply.github.com>
2020-12-06 10:54:22 -0800

    added dateTitle (year/month/day)

--- clock.js
+++ clock.js
@@ -1,11 +1,18 @@
 const clockContainer = document.querySelector(".js-clock"),
-    clockTitle = clockContainer.querySelector("h1");
+    clockTitle = clockContainer.querySelector("h1"),
+    dateTitle = clockContainer.querySelector("h2");
 
 function getTime() {
-    const date = new Date();
-    const minutes = date.getMinutes();
-    const hours = date.getHours();
-    const seconds = date.getSeconds();
+    const time = new Date();
+
+    const year = time.getFullYear();
+    const month = time.getMonth()+1;
+    const day = time.getDate();
+    dateTitle.innerText = `${year}/${month}/${day}`;
+    
+    const minutes = time.getMinutes();
+    const hours = time.getHours();
+    const seconds = time.getSeconds();
     clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${minutes < 10 ? `0${minutes}` : minutes}:${seconds < 10 ? `0${seconds}` : seconds}`;
 }
 
